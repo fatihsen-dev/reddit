9cb2a47663b234d3c7135b0e69259b32e9f89f71
fatihsen-dev <fatihydrm@outlook.com>
2023-11-10 11:03:18 +0300

    add: user unfollow api endpoint

---
+++ src/app/api/un_follow/route.ts
@@ -0,0 +1,86 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import { db } from "~/server/db";
+
+type BodyT = {
+  followedId: string;
+  followerId: string;
+};
+
+const bodySchema = z.object({
+  followedId: z.string({ required_error: "followedId required" }),
+  followerId: z.string({ required_error: "followerId required" }),
+});
+
+export const POST = async (req: Request) => {
+  try {
+    const body = (await req.json()) as BodyT;
+
+    const response = bodySchema.safeParse(body);
+    if (!response.success) {
+      return NextResponse.json(
+        {
+          message: response.error.issues[0]?.message,
+        },
+        { status: 400 },
+      );
+    }
+
+    const isUnFollowed = await db.userFollower.findFirst({
+      where: {
+        followedId: body.followedId,
+        followerId: body.followerId,
+      },
+    });
+
+    if (!isUnFollowed) {
+      return NextResponse.json(
+        {
+          message: "user is not tracked",
+        },
+        { status: 400 },
+      );
+    }
+
+    await db.userFollower.deleteMany({
+      where: {
+        followedId: body.followedId,
+        followerId: body.followerId,
+      },
+    });
+
+    const user = await db.user.findUnique({
+      where: {
+        id: body.followedId,
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        desc: true,
+        email: true,
+        emailVerified: true,
+        image: true,
+        createdAt: true,
+        updatedAt: true,
+        _count: {
+          select: {
+            followers: true,
+            following: true,
+          },
+        },
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(user);
+    }
+    return NextResponse.json({ message: "User not followed" }, { status: 404 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "something went wrong" },
+      { status: 500 },
+    );
+  }
+};
