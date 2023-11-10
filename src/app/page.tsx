63c5c62fa5487110a8e31afbdb351885703e0ee0
fatihsen-dev <fatihydrm@outlook.com>
2023-11-10 11:09:22 +0300

    edit: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useState } from "react";
+import PostCreate from "~/components/PostCreate";
+import TabList from "~/components/TabList";
+import Feed from "./Feed";
+import Friends from "./Friends";
+
 export default function Page() {
-  return <div>Home</div>;
+  const [activeTab, setActiveTab] = useState<"feed" | "friends">("feed");
+
+  return (
+    <div className="grid grid-rows-[auto_1fr] gap-5">
+      <TabList activeTab={activeTab} setActiveTab={setActiveTab} />
+      <PostCreate />
+      {activeTab === "feed" ? <Feed /> : <Friends />}
+    </div>
+  );
 }
