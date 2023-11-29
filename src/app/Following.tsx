1c3b4fc1508e889628572e35ef401274c3d71f91
fatihsen-dev <fatihydrm@outlook.com>
2023-11-29 23:25:08 +0300

    rename: friends -> following

---
+++ src/app/Following.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PostCreate from "~/components/Post/PostCreate";
+
+export default function Following() {
+  return (
+    <>
+      <ul className="grid content-start gap-4 pb-20"></ul>
+    </>
+  );
+}
