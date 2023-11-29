b8f7e2953beeba4be57b35f0c3e6502b0c97dd60
fatihsen-dev <fatihydrm@outlook.com>
2023-11-29 23:38:29 +0300

    edit: update create post validation schema

--- src/validation/createPost.ts
+++ src/validation/createPost.ts
@@ -4,9 +4,9 @@ export const createPostSchema = z.object({
   title: z
     .string({ required_error: "Title required" })
     .min(3, { message: "Title can be a minimum of 3 characters" })
-    .max(50, { message: "Title can be maximum 50 characters" }),
+    .max(80, { message: "Title can be maximum 80 characters" }),
   content: z
     .string()
-    .max(1000, { message: "Content can be maximum 12 characters" })
+    .max(1000, { message: "Content can be maximum 1000 characters" })
     .optional(),
 });
