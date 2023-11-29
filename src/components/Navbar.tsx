12e81dd9c5b69b1f2454f440157238b7a12e9956
fatihsen-dev <fatihydrm@outlook.com>
2023-11-29 23:31:03 +0300

    edit: add search component line

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,21 @@ import NextLink from "next/link";
 import ProfileMenu from "~/components/ProfileMenu";
 import { ThemeToggle } from "~/components/ThemeToggle";
 import { Button } from "~/components/ui/button";
+import { Input } from "./ui/input";
 
 export default function Navbar() {
   const { status } = useSession();
   return (
-    <nav className="sticky top-0 z-20 flex items-center justify-between bg-background py-5">
+    <nav className="sticky top-0 z-20 grid grid-cols-[240px_1fr_240px] items-center justify-between bg-background py-5">
       <div>
         <NextLink href="/">
           <h1 className="text-xl font-semibold">Reddit</h1>
         </NextLink>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex w-full justify-center">
+        <Input className="max-w-xl" placeholder="Search" />
+      </div>
+      <div className="flex items-center justify-end gap-4">
         <ThemeToggle />
         {status === "unauthenticated" ? (
           <NextLink href="/login">
