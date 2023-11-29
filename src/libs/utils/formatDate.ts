e173b7695fb7c9dedb24d9086aaf66d48f26a3f1
fatihsen-dev <fatihydrm@outlook.com>
2023-11-29 23:35:56 +0300

    edit: create date format function

--- src/libs/utils/formatDate.ts
+++ src/libs/utils/formatDate.ts
@@ -1,3 +1,14 @@
+import { DateTime } from "luxon";
+
 export const formatDate = (date: Date) => {
   return new Date(date).toLocaleDateString();
+};
+
+export const timeAgo = (date: string): string | null => {
+  const parsedDate = DateTime.fromISO(date);
+  if (!parsedDate.isValid) {
+    return `Invalid Date`;
+  }
+
+  return parsedDate.toRelative({ style: "short" });
 };
