d17553ff8748ed0fdeb7f3de29a167082cd4296c
fatihsen-dev <fatihydrm@outlook.com>
2023-11-29 23:34:41 +0300

    edit: update left right side menu

--- src/components/LeftSide/index.tsx
+++ src/components/LeftSide/index.tsx
@@ -4,7 +4,7 @@ import PageList from "./PageList";
 export default function LeftSide() {
   return (
     <div className="min-w-[260px]">
-      <div className="sticky top-20 grid w-full content-start gap-6">
+      <div className="sticky top-[6.25rem] grid w-full content-start gap-6">
         <PageList />
         <GroupList />
       </div>
